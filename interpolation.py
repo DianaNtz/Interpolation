2625f7537278b8154985cb3f57a364e57f2b454a
Diana Nitzschke <diana.nitzschke@uni-jena.de>
2025-05-26 15:09:41 +0200

    created grid

--- interpolation.py
+++ interpolation.py
@@ -1,3 +1,15 @@
 """implementation of Barycentric Lagrange Interpolation in 1D and 2D"""
 import numpy as np
 import matplotlib.pyplot as plt
+#create grid
+zmin=0
+zmax=np.pi
+N=21
+
+def grid(xmin,xmax,Nx):
+     x=np.zeros(Nx)
+     cx=(xmax-xmin)/2
+     dx=(xmax+xmin)/2
+     for j in range(0,Nx):
+         x[j]=-np.cos(j*np.pi/(Nx-1))*cx+dx
+     return x
