508bfa5a0746a585ac7068bd65e202a9a592d030
Diana Nitzschke <diana.nitzschke@uni-jena.de>
2025-05-26 15:11:42 +0200

    added interpolation function 1D

--- interpolation.py
+++ interpolation.py
@@ -19,3 +19,33 @@ def f(z):
 
 zz=grid(zmin,zmax,N)
 fzz=f(zz)
+#1D Interpolation
+def omega(zz):
+    N=len(zz)
+    w=np.zeros(N)
+    for i in range(0,N):
+        zi=zz[i]
+        wi=1
+        for j in range(0,i):
+            wi*=zi-zz[j]
+        for j in range(i+1,N):
+            wi*=zi-zz[j]
+        w[i]=1/wi
+    return w
+w=omega(zz)
+
+def p(z,fzz):
+    result=np.zeros(len(z))
+    for i in range (0,len(z)):
+       C1=0
+       C2=0
+       for j in range (0,len(w)):
+          if(np.abs(z[i]-zz[j])<0.00001):
+              C1=1
+              C2=fzz[j]
+              break
+          else:
+             C1=C1+w[j]/(z[i]-zz[j])
+             C2=C2+w[j]/(z[i]-zz[j])*fzz[j]
+       result[i]=C2/C1
+    return result
