cebc819da6edc21a0e18d108f03581fe74d9110b
Diana Nitzschke <diana.nitzschke@uni-jena.de>
2025-05-26 15:10:32 +0200

    added test function for interpolation

--- interpolation.py
+++ interpolation.py
@@ -13,3 +13,9 @@ def grid(xmin,xmax,Nx):
      for j in range(0,Nx):
          x[j]=-np.cos(j*np.pi/(Nx-1))*cx+dx
      return x
+#create test function 1D
+def f(z):
+    return np.exp(-z**2)
+
+zz=grid(zmin,zmax,N)
+fzz=f(zz)
